89c7b45d9b7937ed0ff797efa2eef37bf8cdb6e7
tmorimatsu <tmorimatsu@r.recruit.co.jp>
2018-11-04 00:02:27 +0900

    first commit

---
+++ BridgeHeader.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  
+//
+//  Created by takuyamorimatsu on 2018/10/11.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+#import <SDWebImage/UIImageView+WebCache.h>
+
+
+#endif /* BridgeHeader_h */
